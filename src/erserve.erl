59434d95f98e9bfc84eb6b2d8ee9c52b3283537a
Daniel Eliasson <daniel.eliasson@klarna.com>
2012-07-13 09:51:52 +0200

    Fix typespecs for open functions

--- src/erserve.erl
+++ src/erserve.erl
@@ -25,24 +25,24 @@
 
 
 %%%_* External API -------------------------------------------------------------
--spec close(string()) -> ok | {error, term()}.
+-spec close(term()) -> ok | {error, term()}.
 close(Name) ->
   Pid = gen_server:call(Name, get_pid),
   supervisor:terminate_child(erserve_sup, Pid).
 
--spec eval(string(), string()) -> {ok, term()} | {error, term()}.
+-spec eval(term(), string()) -> {ok, term()} | {error, term()}.
 eval(Name, Expr) ->
   gen_server:call(Name, {eval, Expr}).
 
--spec open(string()) -> string().
+-spec open(term()) -> string().
 open(Name) ->
   open(Name, ?default_host).
 
--spec open(string(), string()) -> string().
+-spec open(term(), string()) -> string().
 open(Name, Host) ->
   open(Name, Host, ?default_port).
 
--spec open(string(), string(), pos_integer()) -> string().
+-spec open(term(), string(), pos_integer()) -> string().
 open(Name, Host, Port) ->
   {ok, _Pid} = supervisor:start_child(erserve_sup, [Name, Host, Port]),
   ok.
