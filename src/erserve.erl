2a7253e9ac6ec0d2a566109a03b50e9a56078b11
Daniel Eliasson <daniel.eliasson@klarna.com>
2012-07-26 16:37:30 +0200

    Restructure erserve to not use gen_servers and pass the socket around instead

--- src/erserve.erl
+++ src/erserve.erl
@@ -1,6 +1,7 @@
 -module(erserve).
 
 -behaviour(application).
+-behaviour(supervisor).
 
 
 %%%_* Exports ------------------------------------------------------------------
@@ -9,9 +10,9 @@
 -export([ close/1
         , eval/2
         , eval_void/2
+        , open/0
         , open/1
         , open/2
-        , open/3
         , set_variable/4
         ]).
 
@@ -20,14 +21,35 @@
         , stop/1
         ]).
 
+%% supervisor callbacks
+-export([ init/1
+        ]).
+
 
 %%%_* Types --------------------------------------------------------------------
--type type() :: int
-              | double
-              | string
-              | array_int
-              | array_double
-              | array_string.
+-opaque connection()   :: gen_tcp:socket().
+-type   r_expression() :: string().
+-type   r_data()       :: integer()
+                        | float()
+                        | string()
+                        | [ integer() ]
+                        | [ float() ]
+                        | [ string() ]
+                        | [ { r_data(), r_data() } ].
+-type   error_code()   :: atom().
+-type   type()         :: int
+                        | double
+                        | string
+                        | array_int
+                        | array_double
+                        | array_string.
+
+-export_type([ connection/0
+             , error_code/0
+             , r_data/0
+             , r_expression/0
+             , type/0
+             ]).
 
 
 %%%_* Local definitions --------------------------------------------------------
@@ -36,45 +58,78 @@
 
 
 %%%_* Connection handling ------------------------------------------------------
--spec close(term()) -> ok | {error, term()}.
-close(Name) ->
-  Pid = gen_server:call(Name, get_pid),
-  supervisor:terminate_child(erserve_sup, Pid).
+-spec close(connection()) -> ok.
+close(Conn) ->
+  gen_tcp:close(Conn).
 
--spec open(term()) -> string().
-open(Name) ->
-  open(Name, ?default_host).
+-spec open() -> connection().
+open() ->
+  open(?default_host, ?default_port).
 
--spec open(term(), string()) -> string().
-open(Name, Host) ->
-  open(Name, Host, ?default_port).
+-spec open(Host :: string()) -> string().
+open(Host) ->
+  open(Host, ?default_port).
 
--spec open(term(), string(), pos_integer()) -> string().
-open(Name, Host, Port) ->
-  {ok, _Pid} = supervisor:start_child(erserve_sup, [Name, Host, Port]),
-  ok.
+-spec open(Name :: string(), Port :: pos_integer()) -> connection().
+open(Host, Port) ->
+  {ok, Conn} = gen_tcp:connect(Host, Port, [ {active, false}
+                                           , binary
+                                           , {packet, raw}
+                                           ]),
+  try
+    ok = erserve_comms:receive_connection_ack(Conn),
+    Conn
+  catch
+    _:_ ->
+      close(Conn),
+      throw(invalid_ack)
+  end.
 
 
 %%%_* Commands -----------------------------------------------------------------
--spec eval(term(), string()) -> {ok, term()} | {error, term(), binary()}.
-eval(Name, Expr) ->
-  gen_server:call(Name, {eval, Expr}).
+-spec eval(connection(), r_expression()) ->
+              {ok, r_data()} | {error, error_code(), binary()}.
+eval(Conn, Expr) ->
+  ok = erserve_comms:send_message(Conn, eval, Expr),
+  erserve_comms:receive_reply(Conn).
 
--spec eval_void(term(), string()) -> ok | {error, term(), binary()}.
-eval_void(Name, Expr) ->
-  gen_server:call(Name, {eval_void, Expr}).
+-spec eval_void(connection(), r_expression()) ->
+                   ok | {error, error_code(), binary()}.
+eval_void(Conn, Expr) ->
+  ok = erserve_comms:send_message(Conn, eval_void, Expr),
+  case erserve_comms:receive_reply(Conn) of
+    {ok, []}               -> ok;
+    {error, AckCode, Rest} -> {error, AckCode, Rest}
+  end.
 
--spec set_variable(term(), string() | atom(), type(), term()) ->
-                      {ok, term()} | {error, term(), binary()}.
-set_variable(Name, VarName, Type, Value) ->
-  gen_server:call(Name, {set_variable, VarName, Type, Value}).
+-spec set_variable(connection(), string() | atom(), type(), r_data()) ->
+                      {ok, r_data()} | {error, error_code(), binary()}.
+set_variable(Conn, Name, Type,   Value) when is_atom(Name) ->
+  set_variable(Conn, atom_to_list(Name), Type, Value);
+set_variable(Conn, Name, int,    Value)                    ->
+  set_variable(Conn, Name, array_int, [Value]);
+set_variable(Conn, Name, double, Value)                    ->
+  set_variable(Conn, Name, array_double, [Value]);
+set_variable(Conn, Name, Type, Value)                      ->
+  ok = erserve_comms:send_message(Conn, {set_variable, Type}, {Name, Value}),
+  erserve_comms:receive_reply(Conn).
 
 
 %%%_* application callbacks ----------------------------------------------------
 -spec start(term(), term()) -> {ok, pid()} | {error, term()}.
 start(_StartType, _Args) ->
-  supervisor:start_link({local, erserve_sup}, erserve_sup, []).
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 -spec stop(term()) -> ok.
 stop(_State) ->
   ok.
+
+
+%%%_* supervisor callbacks -----------------------------------------------------
+-spec init([]) -> term().
+init([]) ->
+  { ok
+  , { { one_for_one, 2, 60 }
+    , []
+    }
+  }.
--- src/erserve_comms.erl
+++ src/erserve_comms.erl
@@ -1,10 +1,9 @@
 -module(erserve_comms).
 
 %%%_* Exports ------------------------------------------------------------------
--export([ eval/2
-        , eval_void/2
-        , receive_connection_ack/1
-        , set_variable/4
+-export([ receive_connection_ack/1
+        , receive_reply/1
+        , send_message/3
         ]).
 
 
@@ -13,166 +12,133 @@
 
 
 %%%_* External API -------------------------------------------------------------
-receive_connection_ack(Sock) ->
-  {ok, Msg} = gen_tcp:recv(Sock, 32),
+-spec receive_connection_ack(erserve:connection()) -> ok.
+receive_connection_ack(Conn) ->
+  {ok, Msg} = gen_tcp:recv(Conn, 32),
   <<"Rsrv", _Version:32, _Protocol:32, _Extra/binary>> = Msg,
   ok.
 
-eval(Sock, Expr) ->
-  ok = send_eval(Sock, Expr),
-  receive_reply(Sock).
-
-eval_void(Sock, Expr) ->
-  ok = send_eval_void(Sock, Expr),
-  case receive_reply(Sock) of
-    {ok, []}               -> ok;
-    {error, AckCode, Rest} -> {error, AckCode, Rest}
+-spec receive_reply(erserve:connection()) ->
+                       {ok, erserve:r_data()} |
+                       {error, erserve:error_code(), binary()}.
+receive_reply(Conn) ->
+  {ok, AckCode} = gen_tcp:recv(Conn, 4),
+  case AckCode of
+    <<?resp_ok>> -> receive_reply_1(Conn);
+    _            -> receive_reply_error(Conn, AckCode)
   end.
 
-set_variable(Sock, Name, Type,   Value) when is_atom(Name) ->
-  set_variable(Sock, atom_to_list(Name), Type, Value);
-set_variable(Sock, Name, int,    Value)                    ->
-  set_variable(Sock, Name, array_int, [Value]);
-set_variable(Sock, Name, double, Value)                    ->
-  set_variable(Sock, Name, array_double, [Value]);
-set_variable(Sock, Name, Type, Value)                      ->
-  ok = send_set_variable(Sock, Name, Type, Value),
-  receive_reply(Sock).
-
-
-%%%_* Internal functions -------------------------------------------------------
-send_eval(Sock, Expr) ->
-  Message = message(eval, Expr),
-  gen_tcp:send(Sock, Message).
-
-send_eval_void(Sock, Expr) ->
-  Message = message(eval_void, Expr),
-  gen_tcp:send(Sock, Message).
-
-send_set_variable(Sock, Name, Type, Value) ->
-  Message = message({set_variable, Type}, {Name, Value}),
-  io:format("Sending~n"),
-  binpp:pprint(iolist_to_binary(Message)),
-  gen_tcp:send(Sock, Message).
+-spec send_message( erserve:connection()
+                  , erserve:type()
+                  , erserve:r_data()) -> ok.
+send_message(Conn, Type, Data) ->
+  Message = message(Type, Data),
+  gen_tcp:send(Conn, Message).
 
 
 %%%_* Data receiving functions -------------------------------------------------
-receive_reply(Sock) ->
-  {ok, AckCode} = gen_tcp:recv(Sock, 4),
-  case AckCode of
-    <<?resp_ok>> -> receive_reply_1(Sock);
-    _            -> receive_reply_error(AckCode, Sock)
-  end.
-
-receive_reply_1(Sock) ->
-  {ok, Msg} = gen_tcp:recv(Sock, 12),
+receive_reply_1(Conn) ->
+  {ok, Msg} = gen_tcp:recv(Conn, 12),
   << Len0:32/integer-little
    , _Offset:32/integer-little
    , Len1:32/integer-little
   >>  = Msg,
   Len = Len0 + (Len1 bsl 31),
-  {ok, receive_data(Sock, Len)}.
+  {ok, receive_data(Conn, Len)}.
 
-receive_reply_error(AckCode, Sock) ->
-  <<2,0,1,ErrCode>> = AckCode,
-  Error             = error_from_code(ErrCode),
-  {ok, Rest}        = gen_tcp:recv(Sock, 0),
+receive_reply_error(Conn, AckCode) ->
+  <<2, 0, 1, ErrCode>> = AckCode,
+  Error                = error_from_code(ErrCode),
+  {ok, Rest}           = gen_tcp:recv(Conn, 0),
   {error, Error, Rest}.
 
-receive_data(Sock, Length) ->
-  case lists:reverse(receive_data(Sock, Length, [])) of
+receive_data(Conn, Length) ->
+  case lists:reverse(receive_data(Conn, Length, [])) of
     [Item] -> Item;
     List   -> List
   end.
 
-receive_data(_Sock, 0, Acc) ->
+receive_data(_Conn, 0, Acc) ->
   Acc;
-receive_data( Sock, Length, Acc) ->
-  {ok, Header} = gen_tcp:recv(Sock, 4),
+receive_data( Conn, Length, Acc) ->
+  {ok, Header} = gen_tcp:recv(Conn, 4),
   << Type:8/integer-little
    , ItemLength:24/integer-little
   >> = Header,
-  {Item, _L} = receive_item(Sock, Type),
+  {Item, _L} = receive_item(Conn, Type),
   NewAcc = [Item|Acc],
   RemainingLength = Length - ItemLength - 4,
-  receive_data(Sock, RemainingLength, NewAcc).
+  receive_data(Conn, RemainingLength, NewAcc).
 
-receive_item(Sock, ?dt_sexp) ->
-  {ok, Header} = gen_tcp:recv(Sock, 4),
+receive_item(Conn, ?dt_sexp) ->
+  {ok, Header} = gen_tcp:recv(Conn, 4),
   << SexpType:8/integer-little
    , SexpLength:24/integer-little
   >> = Header,
-  Item = receive_sexp(Sock, SexpType, SexpLength),
+  Item = receive_sexp(Conn, SexpType, SexpLength),
   {Item, SexpLength + 4}.
 
-receive_sexp(Sock, Type,             Length) when Type > ?xt_has_attr ->
+receive_sexp(Conn, Type,             Length) when Type > ?xt_has_attr ->
   %% SEXP has attributes, so we need to read off the attribute SEXP
   %% before we get to this expression proper
-  {AttrSexp, AttrSexpLength} = receive_item(Sock, ?dt_sexp),
-  Sexp                       = receive_sexp(Sock,
+  {AttrSexp, AttrSexpLength} = receive_item(Conn, ?dt_sexp),
+  Sexp                       = receive_sexp(Conn,
                                             Type - ?xt_has_attr,
                                             Length - AttrSexpLength),
   {AttrSexp, Sexp};
-receive_sexp(Sock, ?xt_str,          Length)                          ->
-  Strings = receive_string_array(Sock, Length),
-  hd(Strings);
-receive_sexp(Sock, ?xt_vector,       Length)                          ->
-  Vector = receive_vector(Sock, Length, []),
-  Vector;
-receive_sexp(Sock, ?xt_symname,      Length)                          ->
-  receive_sexp(Sock, ?xt_str, Length);
-receive_sexp(Sock, ?xt_list_tag,     Length)                          ->
-  TagList = receive_tagged_list(Sock, Length, []),
-  TagList;
-receive_sexp(Sock, ?xt_lang_notag,   Length)                          ->
-  receive_sexp(Sock, ?xt_vector, Length);
-receive_sexp(Sock, ?xt_vector_exp,   Length)                          ->
-  receive_sexp(Sock, ?xt_vector, Length);
-receive_sexp(Sock, ?xt_clos,         Length)                          ->
-  Closure = receive_closure(Sock, Length),
-  Closure;
-receive_sexp(Sock, ?xt_array_int,    Length)                          ->
-  Array = receive_int_array(Sock, Length, []),
-  Array;
-receive_sexp(Sock, ?xt_array_double, Length)                          ->
-  Array = receive_double_array(Sock, Length, []),
-  Array;
-receive_sexp(Sock, ?xt_array_str,    Length)                          ->
-  Array = receive_string_array(Sock, Length),
-  Array.
+receive_sexp(Conn, ?xt_str,          Length)                          ->
+  hd(receive_string_array(Conn, Length));
+receive_sexp(Conn, ?xt_vector,       Length)                          ->
+  receive_vector(Conn, Length, []);
+receive_sexp(Conn, ?xt_symname,      Length)                          ->
+  receive_sexp(Conn, ?xt_str, Length);
+receive_sexp(Conn, ?xt_list_tag,     Length)                          ->
+  receive_tagged_list(Conn, Length, []);
+receive_sexp(Conn, ?xt_lang_notag,   Length)                          ->
+  receive_sexp(Conn, ?xt_vector, Length);
+receive_sexp(Conn, ?xt_vector_exp,   Length)                          ->
+  receive_sexp(Conn, ?xt_vector, Length);
+receive_sexp(Conn, ?xt_clos,         Length)                          ->
+  receive_closure(Conn, Length);
+receive_sexp(Conn, ?xt_array_int,    Length)                          ->
+  receive_int_array(Conn, Length, []);
+receive_sexp(Conn, ?xt_array_double, Length)                          ->
+  receive_double_array(Conn, Length, []);
+receive_sexp(Conn, ?xt_array_str,    Length)                          ->
+  receive_string_array(Conn, Length).
 
-receive_closure(Sock, Length) ->
-  {ok, Closure} = gen_tcp:recv(Sock, Length),
+receive_closure(Conn, Length) ->
+  {ok, Closure} = gen_tcp:recv(Conn, Length),
   Closure.
 
-receive_int_array(_Sock, 0,      Acc) ->
+receive_int_array(_Conn, 0,      Acc) ->
   lists:reverse(Acc);
-receive_int_array( Sock, Length, Acc) ->
-  Int             = receive_int(Sock),
+receive_int_array( Conn, Length, Acc) ->
+  Int             = receive_int(Conn),
   NewAcc          = [Int|Acc],
   RemainingLength = Length - 4,
-  receive_int_array(Sock, RemainingLength, NewAcc).
+  receive_int_array(Conn, RemainingLength, NewAcc).
 
-receive_int(Sock) ->
-  {ok, Data}                = gen_tcp:recv(Sock, 4),
+receive_int(Conn) ->
+  {ok, Data}                = gen_tcp:recv(Conn, 4),
   <<Int:32/integer-little>> = Data,
   Int.
 
-receive_double_array(_Sock, 0,      Acc) ->
+receive_double_array(_Conn, 0,      Acc) ->
   lists:reverse(Acc);
-receive_double_array( Sock, Length, Acc) ->
-  Double          = receive_double(Sock),
+receive_double_array( Conn, Length, Acc) ->
+  Double          = receive_double(Conn),
   NewAcc          = [Double|Acc],
   RemainingLength = Length - 8,
-  receive_double_array(Sock, RemainingLength, NewAcc).
+  receive_double_array(Conn, RemainingLength, NewAcc).
 
-receive_double(Sock) ->
-  {ok, Data}                 = gen_tcp:recv(Sock, 8),
+receive_double(Conn) ->
+  {ok, Data}                 = gen_tcp:recv(Conn, 8),
   <<Double:64/float-little>> = Data,
   Double.
 
-receive_string_array(Sock, Length) ->
-  {ok, Data} = gen_tcp:recv(Sock, Length),
+receive_string_array(Conn, Length) ->
+  {ok, Data} = gen_tcp:recv(Conn, Length),
   %% Strip off '\01'-padding, split on null terminators, strip more padding
   String     = string:strip(binary_to_list(Data), right, 1),
   Strings0   = string:tokens(String, [0]),
@@ -180,23 +146,23 @@ receive_string_array(Sock, Length) ->
                 string:strip(Str, left, 1)
             end, Strings0).
 
-receive_tagged_list(_Sock, 0,      Acc) ->
+receive_tagged_list(_Conn, 0,      Acc) ->
   lists:reverse(Acc);
-receive_tagged_list( Sock, Length, Acc) ->
-  {Value, ValueLength} = receive_item(Sock, ?dt_sexp),
-  {Key,   KeyLength}   = receive_item(Sock, ?dt_sexp),
+receive_tagged_list( Conn, Length, Acc) ->
+  {Value, ValueLength} = receive_item(Conn, ?dt_sexp),
+  {Key,   KeyLength}   = receive_item(Conn, ?dt_sexp),
   Item                 = {Key, Value},
   NewAcc               = [Item|Acc],
   RemainingLength      = Length - KeyLength - ValueLength,
-  receive_tagged_list(Sock, RemainingLength, NewAcc).
+  receive_tagged_list(Conn, RemainingLength, NewAcc).
 
-receive_vector(_Sock, 0,      Acc) ->
+receive_vector(_Conn, 0,      Acc) ->
   lists:reverse(Acc);
-receive_vector( Sock, Length, Acc) ->
-  {Item, UsedLength} = receive_item(Sock, ?dt_sexp),
+receive_vector( Conn, Length, Acc) ->
+  {Item, UsedLength} = receive_item(Conn, ?dt_sexp),
   NewAcc = [Item|Acc],
   RemainingLength = Length - UsedLength,
-  receive_vector(Sock, RemainingLength, NewAcc).
+  receive_vector(Conn, RemainingLength, NewAcc).
 
 
 %%%_* Data sending functions ---------------------------------------------------
